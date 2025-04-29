176d5927fcc80eb13ca535bba8e35bc5db08e639
techhub20 <techdigihub06@gmail.com>
2025-04-29 10:11:35 +0200

    add env

--- Resume-Analyser-project/resumeanalyser-frontend/src/app/services/analyzer.service.ts
+++ Resume-Analyser-project/resumeanalyser-frontend/src/app/services/analyzer.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, BehaviorSubject } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnalyzerService {
   private analysisResult: any;
-  private apiUrl = 'http://localhost:8080/api/upload';
+  private apiUrl = environment.apiUrl + '/upload'; // Use the environment variable for the API URL
+  //'http://localhost:8080/api/upload';
   private resultSource = new BehaviorSubject<any>(null);
   currentResult = this.resultSource.asObservable();
 
